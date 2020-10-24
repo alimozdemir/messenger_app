5b37cc32c55e2c8c02af7f0127e54998f9ac8b2d
lyzerk <alm.ozdmr@gmail.com>
2020-10-24 14:08:04 +0300

    data annotations

--- server/messenger_api/Models/DTOs/Login.cs
+++ server/messenger_api/Models/DTOs/Login.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace messenger_api.Models.DTOs
 {
     public class Login
     {
+        [Required]
         public string UserName { get; set; }
     }
 }
--- server/messenger_api/Models/DTOs/Message.cs
+++ server/messenger_api/Models/DTOs/Message.cs
@@ -1,9 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace messenger_api.Models.DTOs
 {
     public class Message
     {
+        [Required]
         public string FromUserId { get; set; }
+        [Required]
         public string ToUserId { get; set; }
+        [Required]
         public string Text { get; set; }
     }
 }
