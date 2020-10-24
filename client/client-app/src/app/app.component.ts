fc5ae58d1ea7e82e5ea26e21714817901921fffd
lyzerk <alm.ozdmr@gmail.com>
2020-10-24 15:05:08 +0300

    signalr client setup

--- client/client-app/src/app/app.component.ts
+++ client/client-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as signalR from '@microsoft/signalr';
 
 @Component({
   selector: 'app-root',
@@ -9,12 +10,37 @@ export class AppComponent {
   loginVisible: boolean;
 
   userId: string;
+  token: string;
+  connection: signalR.HubConnection;
+
+  activeUserId: string;
+  text: string;
+
+  messages: string[] = [];
 
   ngOnInit() {
     this.loginVisible = true;
   }
 
-  loginOk(user: string) {
+  async loginOk(user: string) {
     this.userId = user;
+    const builder = new signalR.HubConnectionBuilder();
+
+    const connection = builder.withUrl('http://localhost:5000/chathub?access_token=' + this.token).build();
+
+    await connection.start();
+
+    this.connection = connection;
+
+    connection.on('ReceiveMessage', this.receiveMessage);
   }
+
+  receiveMessage(text: string, userId: string) {
+    this.messages.push(text);
+  }
+
+  send() {
+    this.connection.send('SendMessage', this.userId, this.activeUserId, this.text);
+  }
+
 }
